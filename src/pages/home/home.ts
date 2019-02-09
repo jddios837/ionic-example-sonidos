96494dd1e0164e49891a88794f7f0d65203fecae
Juan Gomez <zatkiel837@gmail.com>
2019-02-09 12:57:56 -0500

    add files sonidos Ionic 3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Animal } from "../../intefaces/animal.interface";
+import { ANIMALES } from "../../data/data.animales";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales: Animal[] = [];
 
+  constructor(public navCtrl: NavController) {
+    this.animales = ANIMALES.splice(0);
   }
 
+  reproducir(animal: Animal) {
+    console.log('Animal ', animal);
+
+    let audio = new Audio();
+    audio.src = animal.audio;
+
+    audio.load();
+    audio.play();
+
+    animal.reproduciendo = true;
+
+    setTimeout( () => animal.reproduciendo = false, animal.duracion * 1000);
+  }
 }
